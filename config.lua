b77e3e6113a85e6c4bf3aca43549113a4da7c31f
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-14 14:12:54 +0200

    add object type duck

--- config.lua
+++ config.lua
@@ -1,5 +1,25 @@
 local yarrrconfig = require( "yarrrconfig" )
 
+function create_duck()
+  return yarrrconfig.create_ship(
+    {
+      Tile.new( TileCoordinate.new( -2, 1 ), TileCoordinate.new( 1, -2 ) ),
+      Tile.new( TileCoordinate.new( 2, 0 ), TileCoordinate.new( 3, 0 ) ),
+      Tile.new( TileCoordinate.new( 0, -3 ), TileCoordinate.new( 0, -3 ) ),
+      Tile.new( TileCoordinate.new( 0, -4 ), TileCoordinate.new( 1, -4 ) ),
+    },
+    {
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( port_thruster, TileCoordinate.new( 2, 0 ), yarrrconfig.degrees( 90 ) ),
+      Thruster.new( starboard_thruster, TileCoordinate.new( 2, 0 ), yarrrconfig.degrees( 270 ) ),
+      Canon.new( TileCoordinate.new( 2, 1 ) ),
+      Canon.new( TileCoordinate.new( 2, -1 ) ),
+    })
+end
+
+register_object_factory( "duck", create_duck )
+
 function create_basic_ship()
   return yarrrconfig.create_ship(
     {
