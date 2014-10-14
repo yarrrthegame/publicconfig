4830df6d6a6157ac61d1eb59d41738300e45d305
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-14 15:12:27 +0200

    add interceptor

--- config.lua
+++ config.lua
@@ -34,6 +34,30 @@ function create_basic_ship()
       Canon.new( TileCoordinate.new( 2, 0 ) ),
     })
 end
-
 register_object_factory( "ship", create_basic_ship )
+
+function create_interceptor()
+  return yarrrconfig.create_ship(
+    {
+      Tile.new( TileCoordinate.new( -1, 0 ), TileCoordinate.new( 4, 0 ) ),
+
+      Tile.new( TileCoordinate.new( 0, 2 ), TileCoordinate.new( 0, 1 ) ),
+      Tile.new( TileCoordinate.new( 1, 1 ), TileCoordinate.new( 1, 1 ) ),
+
+      Tile.new( TileCoordinate.new( 0, -1 ), TileCoordinate.new( 0, -2 ) ),
+      Tile.new( TileCoordinate.new( 1, -1 ), TileCoordinate.new( 1, -1 ) ),
+    },
+    {
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( -1, 0 ), yarrrconfig.degrees( 180 ) ),
+      Thruster.new( port_thruster, TileCoordinate.new( 4, 0 ), yarrrconfig.degrees( 90 ) ),
+      Thruster.new( starboard_thruster, TileCoordinate.new( 4, 0 ), yarrrconfig.degrees( 270 ) ),
+      Canon.new( TileCoordinate.new( 1, -1 ) ),
+      Canon.new( TileCoordinate.new( 2, 0 ) ),
+      Canon.new( TileCoordinate.new( 1, 1 ) ),
+    })
+end
+register_object_factory( "interceptor", create_interceptor )
 
