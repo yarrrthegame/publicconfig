cabc687c1e41a08de7831a5a5710b2dfda89dfee
dromie <dromie@gmail.com>
2014-11-01 10:25:44 +0100

    airtou super stable ship added

--- config.lua
+++ config.lua
@@ -133,3 +133,27 @@ end
 
 register_object_factory( "bike", create_bike )
 
+function create_airtou( airtou )
+  yarrrconfig.create_ship(
+    airtou,
+    {
+      Tile.new( TileCoordinate.new( 0,-1 ), TileCoordinate.new( 0,1 ) ),
+      Tile.new( TileCoordinate.new( -1,0 ), TileCoordinate.new( 1,0 ) ),
+      Tile.new( TileCoordinate.new( 1,1), TileCoordinate.new ( -1, -1 ))
+    },
+    {
+      Thruster.new( port_thruster, TileCoordinate.new( 0,0 ), degrees( 330 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( 0,0 ), degrees( 90 ) ),
+      Thruster.new( starboard_thruster, TileCoordinate.new( 0, 0 ), degrees( 210 ) ),
+      Thruster.new( port_thruster, TileCoordinate.new( 0,0 ), degrees( 330 ) ),
+      Thruster.new( main_thruster, TileCoordinate.new( 0,0 ), degrees( 90 ) ),
+      Thruster.new( starboard_thruster, TileCoordinate.new( 0, 0 ), degrees( 210 ) ),
+      Canon.new( TileCoordinate.new( 1, 0 ), degrees( 0 ) ),
+      Canon.new( TileCoordinate.new( 0, 1 ), degrees( 270 ) ),
+      Canon.new( TileCoordinate.new( -1, 0 ), degrees( 180 ) ),
+      Canon.new( TileCoordinate.new( 0, -1 ), degrees( 90 ) ),
+    })
+end
+
+register_object_factory( "airtou", create_airtou )
+
