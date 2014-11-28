7d38ef0c6fce493948477f04235f0399f2b7b351
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-11-28 16:28:00 +0100

    use new updater wrapper from yarrrconfig

--- missions/tutorial.lua
+++ missions/tutorial.lua
@@ -10,7 +10,7 @@ function add_random_checkpoint( mission )
   local till = universe_time() + 300;
 
   mission:add_objective( MissionObjective.new(
-    "Congratulations! You know everything about moving around in yarrr space.  Your last objective is to navigate the ship to position ".. destination.x .. ", ".. destination.y .. " until " .. os.date( "!%T", till ),
+    "Your last objective is to navigate the ship to position ".. destination.x .. ", ".. destination.y .. " until " .. os.date( "!%T", till ),
     function( m )
       return yc.checkpoint( m:id(), destination, 100, till )
     end ) )
@@ -20,43 +20,57 @@ end
 function add_speed_check( mission )
   mission:add_objective( MissionObjective.new(
     [===[You can see your ship in the middle of the screen.  To activate your main engine press the UP arrow button.]===],
-    function( m )
-      if ( not yc.is_slower_than( 50, yc.ship_of_mission( m:id() ) ) ) then
+    yc.wrap_updater(
+      function() end,
+      function( m )
+        if ( not yc.is_slower_than( 50, yc.ship_of_mission( m:id() ) ) ) then
+          return succeeded
+        end
+        return ongoing
+      end,
+      function( m )
         yc.add_instruction( m, "Great! As you can see after an impulse your ship is drifting in a straight line." )
         yc.add_instruction( m, "\"An object either remains at rest or continues to move at a constant velocity, unless acted upon by an external force.\", Newton" )
         add_rotation_check( m )
-        return succeeded
       end
-
-      return ongoing
-    end ) )
+    ) ) )
 end
 
 function add_rotation_check( mission )
   mission:add_objective( MissionObjective.new(
     [===[Now try to spin the ship by using eighter the port ( LEFT ) or the starboard ( RIGHT ) engine.]===],
-    function( m )
-      angular_velocity = yc.ship_of_mission( m:id() ).angular_velocity
-      if ( angular_velocity ~= 0 ) then
+    yc.wrap_updater(
+      function() end,
+      function( m )
+        angular_velocity = yc.ship_of_mission( m:id() ).angular_velocity
+        if ( angular_velocity ~= 0 ) then
+          return succeeded
+        end
+        return ongoing
+      end,
+      function( m )
+        yc.add_instruction( m, "Fabulous! The next step might be a bit challenging, but don't give up.")
         add_stop_check( m )
-        return succeeded
       end
-
-      return ongoing
-    end ) )
+    ) ) )
 end
 
 function add_stop_check( mission )
   mission:add_objective( MissionObjective.new(
-    [===[Fabulous! The next step might be a bit challenging, but don't give up. Try to stop your vessel.]===],
-    function( m )
-      if yc.is_slower_than( 10, yc.ship_of_mission( m:id() ) ) then
-         add_random_checkpoint( m )
-         return succeeded
+    "Try to stop your vessel.",
+    yc.wrap_updater(
+      function() end,
+      function( m )
+        if yc.is_slower_than( 10, yc.ship_of_mission( m:id() ) ) then
+          return succeeded
+        end
+        return ongoing
+      end,
+      function( m )
+        yc.add_instruction( m, "Congratulations! You know everything about moving around in yarrr space." )
+        add_random_checkpoint( m )
       end
-
-      return ongoing
-    end ) )
+    ) ) )
 end
 
 function t.create( mission )
