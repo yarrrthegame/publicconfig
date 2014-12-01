f507ad0569085d5254eff3610084833e089b074d
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-12-01 10:40:21 +0100

    use common objective creator

--- missions/tutorial.lua
+++ missions/tutorial.lua
@@ -3,74 +3,77 @@ t = {}
 local yc = require( "yarrrconfig" )
 
 function add_random_checkpoint( mission )
-  local destination = {
-    x = math.random( -1000, 1000 ),
-    y = math.random( -1000, 1000 ) }
-
+  local destination = yc.random_location_around( { x=0, y=0 }, 1000 )
   local till = universe_time() + 300;
 
-  mission:add_objective( MissionObjective.new(
-    "Your last objective is to navigate the ship to position ".. destination.x .. ", ".. destination.y .. " until " .. os.date( "!%T", till ),
-    function( m )
+  yc.add_objective_to( mission, {
+
+    description = "Your last objective is to navigate the ship to position ".. destination.x .. ", ".. destination.y .. " until " .. os.date( "!%T", till ),
+
+    updater = function( m )
       return yc.checkpoint( m, destination, 100, till )
-    end ) )
+    end } )
+
 end
 
 
 function add_speed_check( mission )
-  mission:add_objective( MissionObjective.new(
-    [===[You can see your ship in the middle of the screen.  To activate your main engine press the UP arrow button.]===],
-    yc.wrap_updater(
-      function() end,
-      function( m )
-        if ( not yc.is_slower_than( 50, yc.ship_of( m ) ) ) then
-          return succeeded
-        end
-        return ongoing
-      end,
-      function( m )
-        yc.add_instruction( m, "Great! As you can see after an impulse your ship is drifting in a straight line." )
-        yc.add_instruction( m, "\"An object either remains at rest or continues to move at a constant velocity, unless acted upon by an external force.\", Newton" )
-        add_rotation_check( m )
+  yc.add_objective_to( mission, {
+
+    description = [===[You can see your ship in the middle of the screen.  To activate your main engine press the UP arrow button.]===],
+
+    updater = function( m )
+      if ( not yc.is_slower_than( 50, yc.ship_of( m ) ) ) then
+        return succeeded
       end
-    ) ) )
+      return ongoing
+    end,
+
+    teardown = function( m )
+      yc.add_instruction( m, "Great! As you can see after an impulse your ship is drifting in a straight line." )
+      yc.add_instruction( m, "\"An object either remains at rest or continues to move at a constant velocity, unless acted upon by an external force.\", Newton" )
+      add_rotation_check( m )
+    end } )
+
 end
 
 function add_rotation_check( mission )
-  mission:add_objective( MissionObjective.new(
-    [===[Now try to spin the ship by using eighter the port ( LEFT ) or the starboard ( RIGHT ) engine.]===],
-    yc.wrap_updater(
-      function() end,
-      function( m )
-        angular_velocity = yc.ship_of( m ).angular_velocity
-        if ( angular_velocity ~= 0 ) then
-          return succeeded
-        end
-        return ongoing
-      end,
-      function( m )
-        yc.add_instruction( m, "Fabulous! The next step might be a bit challenging, but don't give up.")
-        add_stop_check( m )
+  yc.add_objective_to( mission, {
+
+    description = [===[Now try to spin the ship by using eighter the port ( LEFT ) or the starboard ( RIGHT ) engine.]===],
+
+    updater = function( m )
+      angular_velocity = yc.ship_of( m ).angular_velocity
+      if ( angular_velocity ~= 0 ) then
+        return succeeded
       end
-    ) ) )
+      return ongoing
+    end,
+
+    teardown = function( m )
+      yc.add_instruction( m, "Fabulous! The next step might be a bit challenging, but don't give up.")
+      add_stop_check( m )
+    end } )
+
 end
 
 function add_stop_check( mission )
-  mission:add_objective( MissionObjective.new(
-    "Try to stop your vessel.",
-    yc.wrap_updater(
-      function() end,
-      function( m )
-        if yc.is_slower_than( 10, yc.ship_of( m ) ) then
-          return succeeded
-        end
-        return ongoing
-      end,
-      function( m )
-        yc.add_instruction( m, "Congratulations! You know everything about moving around in yarrr space." )
-        add_random_checkpoint( m )
+  yc.add_objective_to( mission, {
+
+    description = "Try to stop your vessel.",
+
+    updater = function( m )
+      if yc.is_slower_than( 10, yc.ship_of( m ) ) then
+        return succeeded
       end
-    ) ) )
+      return ongoing
+    end,
+
+    teardown = function( m )
+      yc.add_instruction( m, "Congratulations! You know everything about moving around in yarrr space." )
+      add_random_checkpoint( m )
+    end } )
+
 end
 
 function t.create( mission )
